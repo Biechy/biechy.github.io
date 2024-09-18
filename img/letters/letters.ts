002732a2ce3b2df0aded24c57891f00295c07c0e
Biechy <Lucas.S.Biechy@gmx.fr>
2024-09-18 15:38:52 +0200

    Updates

---
+++ img/letters/letters.ts
@@ -0,0 +1,32 @@
+interface Letter {
+  name: string;
+  png: string;
+}
+
+export const letters: Letter[] = [
+  {
+    name: "lambda",
+    png: "./img/letters/lambda.png",
+  },
+  {
+    name: "pi",
+    png: "./img/letters/pi.png",
+  },
+  {
+    name: "gamma",
+    png: "./img/letters/gamma.png",
+  },
+  {
+    name: "alpha",
+    png: "./img/letters/alpha.png",
+  },
+  {
+    name: "beta",
+    png: "./img/letters/beta.png",
+  },
+
+  {
+    name: "epsilon",
+    png: "./img/letters/epsilon.png",
+  },
+];
